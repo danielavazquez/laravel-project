8e1f7251a559eecfbff619439125fb102358ada4
Matheus Tontini <tontini92@gmail.com>
2019-06-12 10:38:32 +0200

    Nullable

---
+++ database/migrations/2019_06_12_083124_change_questions_nullable.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeQuestionsNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->string('title')->nullable()->change(); // makes the column nullable
+            $table->longText('text')->nullable()->change(); // makes the column nullable
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
